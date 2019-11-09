dc40ce9b88d227f0293eb3e335f4688b8847d006
yamada <dkymd0712@gmail.com>
2019-11-09 16:30:53 +0900

    練習問題2.2を実施。

---
+++ _study/exercises/2_2.rb
@@ -0,0 +1,13 @@
+class Neko
+  def bark
+    puts "にゃーご"
+  end
+
+  def eat
+    puts "魚大好き"
+  end
+end
+
+neko = Neko.new
+neko.bark
+neko.eat
---
+++ _study/topics_exe.rb
@@ -0,0 +1,19 @@
+# Taroオブジェクトの定義
+class Taro
+  def name
+    name = "山田太郎"
+  end
+end
+
+#Topicオブジェクトの定義
+class Topic
+  def show_name
+    puts "Taroの名前を表示します"
+    taro = Taro.new
+    puts taro.name
+  end
+end
+
+#Topicクラスをインスタンス化してshow_nameメソッドを実行する
+topic = Topic.new
+topic.show_name
